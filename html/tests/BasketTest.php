faae3291379551bd67020452e0e8dde7f7d82e9a
karintotabetai <19652340+karintomania@users.noreply.github.com>
2022-05-22 16:39:43 +0100

    add tests for add function

--- html/tests/BasketTest.php
+++ html/tests/BasketTest.php
@@ -4,7 +4,7 @@ namespace Tests;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use App\Basket;
-use App\Models\{User,UserOffer};
+use App\Models\{User,UserOffer, Product, UserProduct};
 use App\Database\{Migration, Seeder};
 
 class BasketTest extends BaseTest{
@@ -25,14 +25,48 @@ class BasketTest extends BaseTest{
 
 		$basket = $this::$container->get(Basket::class);
 		$basket->init($user);
-
 		// get the saved offers for the user
 		$userOffers = UserOffer::where('user_id', $user->id)
 			->get();
 
 		$this->assertEquals(1, count($userOffers));
 		$userOffer = $userOffers[0];
-		$this->assertEquals(1, $userOffer->user_id);
+		$this->assertEquals($user->id, $userOffer->user_id);
 		$this->assertEquals(0, $userOffer->offer_id);
 	}
+
+	public function testAddSavesProduct(){
+		$user =	User::find(1);
+
+		$basket = $this::$container->get(Basket::class);
+		$basket->init($user);
+
+		$product = Product::find(1);
+
+		$basket->add($product);
+
+		$userProducts = UserProduct::where('user_id', $user->id)->get();		
+
+		$this->assertEquals(1, count($userProducts));
+		$userProduct = $userProducts[0];
+		$this->assertEquals($product->id, $userProduct->product_id);
+
+	}
+
+	public function testAddThrowsExceptionForDuplicateProduct(){
+		$user =	User::find(1);
+
+		$basket = $this::$container->get(Basket::class);
+		$basket->init($user);
+
+		$product = Product::find(1);
+
+		$basket->add($product);
+
+		// throw exception for duplicated product
+		$this->expectException(\InvalidArgumentException::class);
+		$basket->add($product);
+
+	}
+
 }
